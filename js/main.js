cd8a7ef55df4478d8f339e39ce14287a207e0fb9
Angelafc001 <angelafc001@gmail.com>
2021-06-06 20:24:53 -0700

    carousel is working!

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    //owl-carousel
+    $('.site-main .projects-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+});

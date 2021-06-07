e3a6ffa89dd870cfebb4a8f8a0acd05310888639
Angelafc001 <angelafc001@gmail.com>
2021-06-06 22:45:30 -0700

    Done, Just have to clean up tomorrow :)

--- js/main.js
+++ js/main.js
@@ -13,4 +13,22 @@ $(document).ready(function () {
             }
         }
     })
+
+    //sticky-nav
+    let nav_offset_top = $('.header_area').height()+20;
+
+    function navbarFixed(){
+        if($('.header_area').length){
+            $(window).scroll(function(){
+                let scroll = $(window).scrollTop();
+                if(scroll >= nav_offset_top){
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                }else{
+                    $('header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
 });
